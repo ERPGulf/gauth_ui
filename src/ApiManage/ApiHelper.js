36b79766b9752ae5f1fda1f26ae0968b14f116fe
hibarineesh <hrineesh@htsqatar.com>
2023-12-18 12:24:37 +0530

    forgetresetkey API
    forgetresetkey API

--- src/ApiManage/ApiHelper.js
+++ src/ApiManage/ApiHelper.js
@@ -139,3 +139,30 @@ export const createUser = async (name, userEmail, mobilePhone, password) => {
     return Promise.reject(error);
   }
 };
+
+
+export const generateResetPasswordKey = async () => {
+  try {
+    const accessToken = localStorage.getItem("access_token");
+
+    const formData = new URLSearchParams();
+    formData.append('user', 'hiba@gmail,com'); // Use the passed email parameter
+
+    // Make the POST request
+    const { data } = await instance.post(
+      'auction_app.gauth.g_generate_reset_password_key',
+      formData.toString(), // Convert FormData to a URL-encoded string
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    return Promise.resolve(data);
+  } catch (error) {
+    console.error(`Error creating reset key: ${error}`);
+    return Promise.reject(error);
+  }
+};
--- src/screens/forgetpassword/Forgetpassword.jsx
+++ src/screens/forgetpassword/Forgetpassword.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { generateToken, generateResetPasswordKey } from '../../ApiManage/ApiHelper';
 
-export const Forgetpassword = () => {
+const Forgetpassword = () => {
+
+  const handleClick = async () => {
+    try {
+      // Call generateToken
+      generateToken()
+
+      // Call generateResetPasswordKey
+      const response = await generateResetPasswordKey();
+      console.log('Reset Password Key Response:', response);
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div>Forgetpassword</div>
-  )
-}
+    <div className="container">
+      <h1 style={{ color: 'black' }}>hi</h1>
+      <button onClick={handleClick}>Generate Token and Reset Key</button>
+      <p>Reset Key: {resetKey}</p>
+    </div>
+  );
+};
+
+export default Forgetpassword;
