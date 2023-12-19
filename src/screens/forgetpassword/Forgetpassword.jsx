846ea13d9a94ffb8f4297736aac9a20ab4d6956f
hibarineesh <hrineesh@htsqatar.com>
2023-12-19 12:09:13 +0530

    update ApiHelper
    update ApiHelper

--- src/screens/forgetpassword/Forgetpassword.jsx
+++ src/screens/forgetpassword/Forgetpassword.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import { generateToken, generateResetPasswordKey, checkPasswordStrength } from '../../ApiManage/ApiHelper';
-import instance from '../../ApiManage/Axios';
+import { generateToken, generateResetPasswordKey, checkPasswordStrength, updatePassword } from '../../ApiManage/ApiHelper';
+
 
 const ForgetPassword = () => {
   const [email, setEmail] = useState('');
@@ -38,32 +37,22 @@ const ForgetPassword = () => {
 
   const handlePasswordReset = async (e) => {
     e.preventDefault();
-
+  
     try {
       // Now, call checkPasswordStrength with the reset key and input password
       const strengthResponse = await checkPasswordStrength(newPassword);
       console.log('Validation:', strengthResponse.data.message.feedback.password_policy_validation_passed);
-
+  
       // Check if password validation passed
       if (strengthResponse && strengthResponse.data && strengthResponse.data.message.feedback.password_policy_validation_passed) {
         // Call updatePassword function
-        const updatePasswordResponse = await instance.post(
-          `frappe.core.doctype.user.user.update_password`,
-          {
-            new_password: newPassword,
-            key: resetKey,
-          },
-          {
-            headers: {
-              'Cookie': 'full_name=Mumtaz%2032; sid=834d08adda118bf4a9761bade5ab686f4afcdd40db680ff103663ea7; system_user=yes; user_id=mumtaz32%40erpgulf.com; user_image=',
-            },
-          }
-        );
-
+        const updatePasswordResponse = await updatePassword(newPassword, resetKey);
+          
+  
         console.log('Update Password Response:', updatePasswordResponse);
-
+  
         // Check if the update was successful
-        if (updatePasswordResponse && updatePasswordResponse.data ) {
+        if (updatePasswordResponse && updatePasswordResponse.data) {
           setUpdateSuccess(true);
         } else {
           setUpdateSuccess(false);
@@ -76,6 +65,7 @@ const ForgetPassword = () => {
       console.error('Error during password update:', error);
     }
   };
+  
 
   return (
     <div className="container">
@@ -115,7 +105,17 @@ const ForgetPassword = () => {
           <p></p>
           <button className='continue' type="submit">Reset Password</button>
           {validationError && <p>{validationError}</p>}
-          {updateSuccess && <p>Password updated successfully!</p>}
+          {updateSuccess && 
+          <p 
+          style={{
+            color:'black',
+            marginLeft:'38px',
+            marginTop:'50px',
+            fontSize:'23px',
+          }}
+            >
+              Password updated successfully!
+            </p>}
         </form>
       )}
     </div>
